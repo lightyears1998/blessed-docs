e9bbb5ec24e0e3aa18c202f5472da41d03565ac9
lightyears1998 <lightyears1998@hotmail.com>
2020-02-18 11:24:39 +0800

    Use CI to check docs source update

---
+++ docs-source-updchck.py
@@ -0,0 +1,20 @@
+# Run this script to check update of docs source defined in data-source.yml
+
+import sys
+import requests
+import yaml
+
+sources = yaml.safe_load(open("./docs-source.yml", encoding="utf8").read())
+
+all_source_is_up_to_date = True
+for source in sources:
+    if source['enable']:
+        uri = source['uri']
+        docs_master = requests.get(uri.replace('{commit-id}', 'master')).text
+        docs_last_update = requests.get(uri.replace('{commit-id}', source['commit-id-of-last-update'])).text
+        if docs_master != docs_last_update:
+            print(f'docs of {source["name"]} is outdated.')
+            all_source_is_up_to_date = False
+
+if not all_source_is_up_to_date:
+    sys.exit(1)
